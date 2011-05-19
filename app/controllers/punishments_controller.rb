3d71d16aa1650118f2026c43afb59e681409e146
chengdh <cheng.donghui@gmail.com>
2011-05-19 16:31:48 +0800

    fixed some punishment bug

--- app/controllers/punishments_controller.rb
+++ app/controllers/punishments_controller.rb
@@ -14,6 +14,7 @@ class PunishmentsController < BaseController
   def show
     @punishment = Punishment.find(params[:id])
     respond_to do |format|
+      format.html
       format.js {render :partial => "export_excel"}
     end
   end
