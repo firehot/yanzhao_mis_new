bc434a15c321578f7750019804aa73ba51c67024
chengdh <cheng.donghui@gmail.com>
2011-07-24 22:06:50 +0800

    fixed config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,4 @@
 # Be sure to restart your server when you modify this file
-if Gem::VERSION >= "1.3.6"
-  module Rails
-    class GemDependency
-      def requirement
-        r = super
-        (r == Gem::Requirement.default) ? nil : r
-      end
-    end
-  end
-end
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require 'thread'
