6a8a757df0385da7ccc01f1d8f38c7b664da51e9
chengdh <cheng.donghui@gmail.com>
2011-10-12 19:06:27 +0800

    change some bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :branch,:master
 #role :app, "your app-server here"                          # This may be the same as your `Web` server
 #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
-server "192.168.0.99",:app,:web,:db,:primary => true
+server "bg.yanzhaowuliu.com",:app,:web,:db,:primary => true
 
 set :user,"root"
 set :use_sudo,false
