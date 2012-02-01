a89aa0c4a2bc455245f96104775eb019918fe45e
chengdh <cheng.donghui@gmail.com>
2012-02-01 18:19:30 +0800

    修改了settlements权限问题

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120201062817) do
+ActiveRecord::Schema.define(:version => 20120201071020) do
 
   create_table "address_book_lines", :force => true do |t|
     t.string   "name",            :limit => 60,                    :null => false
