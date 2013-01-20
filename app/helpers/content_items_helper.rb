8bd18c3859bae0eab4878e5ed0905331c9e1fbfd
chengdh <cheng.donghui@gmail.com>
2013-01-20 19:45:55 +0800

    修改了组织机构界面的设计

--- app/helpers/content_items_helper.rb
+++ app/helpers/content_items_helper.rb
@@ -1,2 +1,7 @@
 module ContentItemsHelper
+  #根据content_class_code获取content_class_name
+  def content_class_name(content_class_code)
+    content_class = ContentClass.find_by_code(content_class_code)
+    content_class.try(:name)
+  end
 end
