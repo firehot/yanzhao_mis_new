8ff5e3d5775107e1b9e8128de4fba7e48ef503f9
chengdh <cheng.donghui@gmail.com>
2013-03-22 11:04:06 +0800

    修改了确认清单及核销清单状态显示的几个错误

--- app/helpers/confirms_helper.rb
+++ app/helpers/confirms_helper.rb
@@ -1,9 +1,7 @@
 module ConfirmsHelper
-  def bill_operate_state_desc(state)
+  def confirm_state_desc(state)
     ret = ""
-    ret = "待确认" if state.eql?('DR')
-    ret = "已确认" if state.eql?('CO')
-    ret = "已核销" if state.eql?('PO')
+    ret = "待确认" if state.eql?('DR') or state.eql?('CO')
     ret = "已审核" if state.eql?('OK')
     ret
   end
--- app/helpers/post_infos_helper.rb
+++ app/helpers/post_infos_helper.rb
@@ -1,2 +1,9 @@
 module PostInfosHelper
+  def post_info_state_desc(state)
+    ret = ""
+    ret = "已核销" if state.eql?('DR') or state.eql?('PO')
+    ret = "已审核" if state.eql?('OK')
+    ret
+  end
+
 end
