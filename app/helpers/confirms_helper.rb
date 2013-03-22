a4cbd0452d1e25a65eed261178380fb20f6271b3
chengdh <cheng.donghui@gmail.com>
2013-03-22 10:49:52 +0800

    修改了confirm状态的错误

--- app/helpers/confirms_helper.rb
+++ app/helpers/confirms_helper.rb
@@ -1,6 +1,7 @@
 module ConfirmsHelper
   def bill_operate_state_desc(state)
-    "待审核" if state.eql?('DR')
+    "待确认" if state.eql?('DR')
+    "已确认" if state.eql?('CO')
     "已审核" if state.eql?('OK')
   end
 end
