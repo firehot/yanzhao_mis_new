1ca0fa31e906b764368a67f03e7039a163ffebbe
chengdh <cheng.donghui@gmail.com>
2013-03-22 11:20:18 +0800

    修改了状态显示的错误

--- app/helpers/confirms_helper.rb
+++ app/helpers/confirms_helper.rb
@@ -1,7 +1,7 @@
 module ConfirmsHelper
   def confirm_state_desc(state)
     ret = ""
-    ret = "待确认" if state.eql?('DR') or state.eql?('CO')
+    ret = "已确认" if state.eql?('DR') or state.eql?('CO')
     ret = "已审核" if state.eql?('OK')
     ret
   end
