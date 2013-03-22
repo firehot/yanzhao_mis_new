963eb6c3973804f367943567ee82a28edcbac271
chengdh <cheng.donghui@gmail.com>
2013-03-22 10:55:02 +0800

    修改了状态显示的一个BUG

--- app/helpers/confirms_helper.rb
+++ app/helpers/confirms_helper.rb
@@ -1,7 +1,10 @@
 module ConfirmsHelper
   def bill_operate_state_desc(state)
-    "待确认" if state.eql?('DR')
-    "已确认" if state.eql?('CO')
-    "已审核" if state.eql?('OK')
+    ret = ""
+    ret = "待确认" if state.eql?('DR')
+    ret = "已确认" if state.eql?('CO')
+    ret = "已核销" if state.eql?('PO')
+    ret = "已审核" if state.eql?('OK')
+    ret
   end
 end
